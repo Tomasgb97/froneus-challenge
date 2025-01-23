b49bbbc83fa49701e9ba2c01ffb012863e0eec09
Tomasgb97 <tomasgomezbelcastro@gmail.com>
2025-01-23 10:34:25 -0300

    feat(Header): Refactor header layout and update styling for improved design

--- src/app/components/layout/header/Header.tsx
+++ src/app/components/layout/header/Header.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Header: React.FC = () => {
     return (
-        <header className='w-full bg-gray-400/40 shadow-md shadow-white/20 fixed flex justify-center'>
-            <h1 className='font-bold text-white'>Welcome to Froneus Challenge</h1>
+        <header className='w-full fixed flex justify-start'>
+            <a className='cursor-pointer select-none h-full bg-gray-400/40 shadow-md shadow-white/20 px-5'>
+          
+            <h1 className='font-bold text-white'>Froneus</h1>
+           
+            </a>
+            
+           
         </header>
     );
 };
