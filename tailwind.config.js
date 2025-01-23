3e2ba74d215994d9c4da6ff51007305c1c84520f
Tomasgb97 <tomasgomezbelcastro@gmail.com>
2025-01-23 13:12:12 -0300

    chore(tailwind.config.js): update color palette and improve formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,25 @@
 export default {
-  content: ["./src/**/*.tsx", "./src/**/*.css"],
-  plugins: [require("@tailwindcss/forms")],
+  content: ['./src/**/*.tsx', './src/**/*.css'],
+  plugins: [require('@tailwindcss/forms')],
   theme: {
     extend: {
-      colors:{
-        'primary':{
-          
-
-        }
+      colors: {
+        primary: {
+          50: '#e6f7ff',
+          100: '#bae7ff',
+          200: '#91d5ff',
+          300: '#69c0ff',
+          400: '#40a9ff',
+          500: '#1890ff',
+          600: '#096dd9',
+          700: '#0050b3',
+          800: '#003a8c',
+          900: '#002766',
+        },
       },
       fontFamily: {
         Figtree: ['Figtree', 'serif'],
-      }
-    }
-  }
+      },
+    },
+  },
 };
