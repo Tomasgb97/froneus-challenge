8700a35dc04f207b951d2f2475ba046c517750b3
Tomasgb97 <tomasgomezbelcastro@gmail.com>
2025-01-23 10:25:38 -0300

    feat(Header, ui): Update header styling for improved visibility and fix positioning

--- src/app/components/layout/header/Header.tsx
+++ src/app/components/layout/header/Header.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Header: React.FC = () => {
     return (
-        <header className='w-full bg-gray-400/40 shadow-md shadow-white/20 absolute flex justify-center'>
-            <h1>Welcome to Froneus Challenge</h1>
+        <header className='w-full bg-gray-400/40 shadow-md shadow-white/20 fixed flex justify-center'>
+            <h1 className='font-bold text-white'>Welcome to Froneus Challenge</h1>
         </header>
     );
 };
