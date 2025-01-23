04aea96a03aacf688872ec1e3af0a6af3f935595
Tomasgb97 <tomasgomezbelcastro@gmail.com>
2025-01-23 10:38:12 -0300

    UI(Header): Update header layout to use flex justify-between for better alignment

--- src/app/components/layout/header/Header.tsx
+++ src/app/components/layout/header/Header.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Header: React.FC = () => {
     return (
-        <header className='w-full fixed flex justify-start'>
+        <header className='w-full fixed flex justify-between'>
             <a className='cursor-pointer select-none h-full bg-gray-400/40 shadow-md shadow-white/20 px-5'>
-          
             <h1 className='font-bold text-white'>Froneus</h1>
-           
             </a>
-            
-           
+
+            <nav>
+                
+            </nav>
         </header>
     );
 };
