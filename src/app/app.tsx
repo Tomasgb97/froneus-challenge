58aecf598ac6a32559944cc7564a602b06d3faf5
Tomasgb97 <tomasgomezbelcastro@gmail.com>
2025-01-23 10:16:13 -0300

    feat(Ui): Add Header component and integrate it into the App layout

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,17 @@
+import Header from "./components/layout/header/Header";
 
 
 
 function App() {
   return (
+    <>
+    <Header></Header>
     <main>
       
      
-    </main>
+      </main>
+      </>
+    
   );
 }
 
---
+++ src/app/components/layout/header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+    return (
+        <header className='w-full bg-gray-400/40 shadow-md shadow-white/20 absolute flex justify-center'>
+            <h1>Welcome to Froneus Challenge</h1>
+        </header>
+    );
+};
+
+export default Header;
