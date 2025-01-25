5677d639eccd8f11891271b9121227cee4649055
Tomasgb97 <tomasgomezbelcastro@gmail.com>
2025-01-25 10:57:12 -0300

    feat(Associates): add InfoDialog for displaying selected receiver details

--- src/app/components/campaigns/campaignAssociates/Associates.tsx
+++ src/app/components/campaigns/campaignAssociates/Associates.tsx
@@ -1,7 +1,9 @@
 import { useReceiverStore } from '@app/stores/recieversStore';
 import { Campaign } from '@app/types/campaigns/campaign';
 import { ListBox } from 'primereact/listbox';
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
+import { Dialog } from 'primereact/dialog';
+import { Person } from '@app/types/person/person';
 
 interface AssociatesProps {
   campaign: Campaign;
@@ -11,27 +13,48 @@ const Associates: React.FC<AssociatesProps> = ({
   campaign,
 }: AssociatesProps) => {
   const { receivers } = useReceiverStore();
-
+  const [selectedReceiver, setSelectedReceiver] = useState<Person | null>(null);
   const associatedRecievers = useMemo(() => {
     return receivers.filter((reciever) =>
       reciever.associatedCampaigns.includes(campaign.id)
     );
   }, [campaign, receivers]);
 
+  const InfoDialog = () => {
+    return (
+      <Dialog
+        className="md:min-w-[600px] md:aspect-[16/3]"
+        header={`${selectedReceiver?.name}  ${selectedReceiver?.surname}`}
+        visible={selectedReceiver !== null}
+        onHide={() => {
+          setSelectedReceiver(null);
+        }}
+      >
+        <span className="font-bold ">Teléfono: </span>{' '}
+        <p className="text-primary-300">{selectedReceiver?.phone}</p>
+      </Dialog>
+    );
+  };
+
   return (
     <div>
-      <h1>Campaign Associates</h1>
-      <div className="card flex justify-content-center">
+      <InfoDialog />
+      <h1 className="text-primary-300">Asociados a la campaña</h1>
+      <div className=" flex justify-content-center">
         <ListBox
           filter
+          onChange={(e) => {
+            console.log(e);
+            setSelectedReceiver(e.value);
+          }}
           options={associatedRecievers}
           itemTemplate={(option) => (
-            <div>
+            <div className="hover:text-primary-300">
               {option.name} {option.surname}
             </div>
           )}
           filterBy="name,surname"
-          className="w-full md:w-14rem"
+          className="w-full"
         />
       </div>
     </div>
