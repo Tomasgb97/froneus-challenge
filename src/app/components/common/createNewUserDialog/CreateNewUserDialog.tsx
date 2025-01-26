80ac6f08062aa9f8a3c5ca1b1cd13bc0eddb6646
Tomasgb97 <tomasgomezbelcastro@gmail.com>
2025-01-26 11:38:18 -0300

    feat(CreateNewUserDialog): refactor dialog to use custom hook for new user creation

--- src/app/components/common/createNewUserDialog/CreateNewUserDialog.tsx
+++ src/app/components/common/createNewUserDialog/CreateNewUserDialog.tsx
@@ -1,65 +1,18 @@
+import useCreateNewReceiver from '@hooks/createNewReceiverHook';
+import { Dialog } from 'primereact/dialog';
 import React from 'react';
-import {
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  Button,
-  TextField,
-} from '@material-ui/core';
 
-interface CreateNewUserDialogProps {
-  open: boolean;
-  onClose: () => void;
-  onCreate: (username: string, email: string) => void;
-}
-
-const CreateNewUserDialog: React.FC<CreateNewUserDialogProps> = ({
-  open,
-  onClose,
-  onCreate,
-}) => {
+const CreateNewUserDialog: React.FC = ({}) => {
+  const { setShowNewUserDialog } = useCreateNewReceiver();
   const [username, setUsername] = React.useState('');
   const [email, setEmail] = React.useState('');
 
-  const handleCreate = () => {
-    onCreate(username, email);
-    setUsername('');
-    setEmail('');
-    onClose();
-  };
-
   return (
-    <Dialog open={open} onClose={onClose}>
-      <DialogTitle>Create New User</DialogTitle>
-      <DialogContent>
-        <TextField
-          autoFocus
-          margin="dense"
-          label="Username"
-          type="text"
-          fullWidth
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <TextField
-          margin="dense"
-          label="Email"
-          type="email"
-          fullWidth
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={onClose} color="primary">
-          Cancel
-        </Button>
-        <Button onClick={handleCreate} color="primary">
-          Create
-        </Button>
-      </DialogActions>
-    </Dialog>
+    <Dialog
+      onHide={() => {
+        setShowNewUserDialog(false);
+      }}
+    ></Dialog>
   );
 };
 
